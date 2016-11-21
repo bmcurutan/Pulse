7be03b7263950e61cc407dc1074ae33cf4784bf0
Bianca Curutan <bmcurutan@gmail.com>
2016-11-20 23:17:10 -0800

    initial commit - pulse graph

---
+++ Pulse/GraphViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GraphViewController.swift
+//  Pulse
+//
+//  Created by Bianca Curutan on 11/20/16.
+//  Copyright © 2016 ABI. All rights reserved.
+//
+
+import SwiftChart
+import UIKit
+
+class GraphViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    func heightForView() -> CGFloat {
+        return 200 // TODO
+    }
+
+}
